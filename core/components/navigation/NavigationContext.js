348e71ccb158fab835782572dd7bc7303f12b27d
PabRubio <pablo.rubio.punal@gmail.com>
2026-05-02 19:41:39 +0100

    fix import

--- core/components/navigation/NavigationContext.js
+++ core/components/navigation/NavigationContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import { supabase } from '../../supabase/services/supabase';
+import { supabase } from '../../../supabase/services/supabase';
 
 const NavigationContext = createContext();
 
